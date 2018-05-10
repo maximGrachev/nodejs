3aa6b1b01fb5a734374d208420b08cd1ddc8d9a5
Maxim Grachev <mxmgrachev@gmail.com>
2018-05-10 10:40:22 +0300

    first request/response server

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 var http = require("http");
+var url = require('url');
 
-function start() {
+function start(route, handle) {
     function onRequest(request, response) {
-        console.log("Request recieved")
+        var pathname = url.parse(request.url).pathname;
+        console.log("Request for " + pathname + " recieved")
+
         response.writeHead(200, { "Content-Type": "text/plain" });
-        response.write("Hello World");
+        var content = route(handle, pathname);
+        response.write(content);
         response.end();
     }
+
     http.createServer(onRequest).listen(8888);
     console.log("Server has started.");
 }
